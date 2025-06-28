e889b8697c948b5818fa112f98c3c859fb02ba35
moraislaila <nataliacuchi27@gmail.com>
2025-06-28 14:51:06 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,7 @@
+valor1 = float (input("digite seu primeiro numero"))
+valor2 = float(input("digite seu segundo numero"))
+valor3 = float (input("digite seu terceiro numero"))
+media = (valor1+valor2+valor3) / 3
+
+print(f"sua media é igual é: {media}")
+ 
