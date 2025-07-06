be41b92af8342066976d882aeb56b1c9d55a052d
moraislaila <nataliacuchi27@gmail.com>
2025-07-06 17:52:54 -0300

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,8 @@
+livro1 =  float (input("digite o valor do primeiro livro"))
+livro2 = float (input("digite o valor do segundo livro"))
+livro3 = float (input("digite o valor do terceiro livro"))
+compra = (livro1 + livro2 + livro3) 
+
+valor = (15/100) * compra
+
+print(f"o total da sua compra foi de {compra}, porem com um desconto de {valor}")
